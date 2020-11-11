0da1a1e93926864a8a028f102c94ca9b7e1ee0d1
Jaden <diogenes_mj@hotmail.com>
2020-11-11 11:24:20 -0400

    fix days

--- src/Components/Qoute/index.js
+++ src/Components/Qoute/index.js
@@ -3,11 +3,9 @@ import { getDayWithWeekend } from "../../Util";
 import Style from "./Style.module.css";
 
 export function Qoute(params) {
-  
-
   return (
     <div className={Style.container}>
-      <p className={Style.title}>{getDayWithWeekend(24)}</p>
+      <p className={Style.title}>{getDayWithWeekend(23)}</p>
     </div>
   );
 }
--- src/Util/index.js
+++ src/Util/index.js
@@ -2,13 +2,19 @@ const dayOfWeeks = ["Mon", "Tue", "Wed", "Tue", "Fri"];
 
 export function getDayWithWeekend(dayLimit) {
   let countDay = 0;
-  let currentDay = new Date()
+  let currentDay = new Date();
   let leastDay = dayLimit - parseInt(new Date().getDate());
 
-  for (let i = 0; i < leastDay.length; i++) {
-      
-      new Date()
+  for (let i = 1; i <= leastDay; i++) {
+    let temp = new Date();
+
+    temp.setDate(temp.getDate() + i);
+    
+    if (temp.getDay() === 0 || temp.getDay() === 6) {
+      leastDay -= 1;
+    }
   }
 
-  return -1 * leastDay;
+    return -1 * leastDay;
+  
 }
